f457e0905cccb7cdfd6ca9e00ea1a221a0aa7a8c
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2023-02-07 00:56:25 -0300

    v1.2.8

--- src/__test__/province.test.ts
+++ src/__test__/province.test.ts
@@ -1,14 +1,47 @@
 import {describe, expect, test} from '@jest/globals';
 import {Province} from '../index';
 
-describe('Pronvice Modules', () => {
-  test('get all Schools From Mendoza', () => {
-  	const provinceSchools = Province.getSchoolsOfProvince("Mendoza")
-  	console.log(provinceSchools)
-    expect(provinceSchools).not.toBeNull();
+describe('Esculas por provincia', () => {
+
+  test('todas las provincias', () => {
+    const provincias = Province.getProvinces()
+    console.log(provincias)
+    expect(provincias.length).not.toBe(0);
   });
 
-  test('get all Schools From Mendoza', () => {
-    expect(Province.getSchoolsOfProvince("Mendoza").length).toBe(406);
-  });
+
+  // test('todas las Escuelas de Mendoza', () => {
+  // 	const provinceSchools = Province.getSchoolsOfProvince("Mendoza")
+  //   expect(provinceSchools).not.toBeNull();
+  // });
+
+  
+
+  // test('hay escuelas en Mendoza', () => {
+  //   expect(Province.getSchoolsOfProvince("Mendoza").length).not.toBe(0);
+  // });
+
+
+  // test('hay escuelas en MENDOZA', () => {
+  //   expect(Province.getSchoolsOfProvince("MENDOZA").length).not.toBe(0);
+  // });
+
+  // test('hay escuelas en mendoza', () => {
+  //   expect(Province.getSchoolsOfProvince("mendoza").length).not.toBe(0);
+  // });
+
+
+
+
+
+  // test('hay escuelas en San Rafael Mendoza', () => {
+  //   expect(Province.getSchoolsOfProvinceAndLocality("Mendoza","SAN RAFAEL").length).not.toBe(0);
+  // });
+
+
+  // test('no hay escuelas para una provincia que no existe', () => {
+  // 	const schools: any = Province.getSchoolsOfProvince("San Rafael")
+
+  // 	expect(schools.length).toEqual(0);
+  // });
 });
--- src/province.ts
+++ src/province.ts
@@ -1,11 +1,40 @@
 import schoolsList from './assets/allSchools.json';
+import pronvinceList from './assets/provinces.json';
+import { ISchool } from './interface';
+import _ from 'lodash';
+
+function getProvinces() {
+	return pronvinceList;
+}
 
 
-function getSchoolsOfProvince(province_name: string) {
+function getSchoolsOfProvince(province_name: string) : ISchool[] {
 	if (!province_name) return [];
 
+	let province_name_cap = _.startCase( _.toLower(province_name) );
+
+	console.log(province_name_cap)
+
 	const schools = schoolsList.data.filter((value:{jurisdiction: string})=>{
-		return value.jurisdiction === province_name
+		return value.jurisdiction === province_name_cap
+	});
+
+
+	return schools;
+}
+
+function getSchoolsOfProvinceAndLocality(province_name: string, locality: string) : ISchool[] {
+	if (!province_name) return [];
+	if (!locality) return [];
+
+	let province_name_cap = _.startCase( _.toLower(province_name) );
+	let locality_cap = _.startCase( _.toLower(locality) );
+
+	console.log(province_name_cap)
+	console.log(locality_cap)
+
+	const schools = schoolsList.data.filter((value:{jurisdiction: string, locality:string})=>{
+		return value.jurisdiction === province_name_cap && value.locality === locality_cap
 	});
 
 
@@ -13,6 +42,9 @@ function getSchoolsOfProvince(province_name: string) {
 }
 
 
+
 export default {
+	getProvinces,
 	getSchoolsOfProvince,
+	getSchoolsOfProvinceAndLocality
 };
