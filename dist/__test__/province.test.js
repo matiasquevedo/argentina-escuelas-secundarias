486ab5badb96cc78e667ee1751cff6a3c87a4fd4
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2023-02-08 13:57:29 -0300

    v.2.2.8

--- dist/__test__/province.test.js
+++ dist/__test__/province.test.js
@@ -2,14 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const globals_1 = require("@jest/globals");
 const index_1 = require("../index");
-(0, globals_1.describe)('Pronvice Modules', () => {
-    (0, globals_1.test)('get all Schools From Mendoza', () => {
-        const provinceSchools = index_1.Province.getSchoolsOfProvince("Mendoza");
-        console.log(provinceSchools);
-        (0, globals_1.expect)(provinceSchools).not.toBeNull();
+(0, globals_1.describe)('Esculas por provincia', () => {
+    (0, globals_1.test)('todas las provincias', () => {
+        const provincias = index_1.Province.getProvinces();
+        console.log(provincias);
+        (0, globals_1.expect)(provincias.length).not.toBe(0);
     });
-    (0, globals_1.test)('get all Schools From Mendoza', () => {
-        (0, globals_1.expect)(index_1.Province.getSchoolsOfProvince("Mendoza").length).toBe(406);
-    });
+    // test('todas las Escuelas de Mendoza', () => {
+    // 	const provinceSchools = Province.getSchoolsOfProvince("Mendoza")
+    //   expect(provinceSchools).not.toBeNull();
+    // });
+    // test('hay escuelas en Mendoza', () => {
+    //   expect(Province.getSchoolsOfProvince("Mendoza").length).not.toBe(0);
+    // });
+    // test('hay escuelas en MENDOZA', () => {
+    //   expect(Province.getSchoolsOfProvince("MENDOZA").length).not.toBe(0);
+    // });
+    // test('hay escuelas en mendoza', () => {
+    //   expect(Province.getSchoolsOfProvince("mendoza").length).not.toBe(0);
+    // });
+    // test('hay escuelas en San Rafael Mendoza', () => {
+    //   expect(Province.getSchoolsOfProvinceAndLocality("Mendoza","SAN RAFAEL").length).not.toBe(0);
+    // });
+    // test('no hay escuelas para una provincia que no existe', () => {
+    // 	const schools: any = Province.getSchoolsOfProvince("San Rafael")
+    // 	expect(schools.length).toEqual(0);
+    // });
 });
 //# sourceMappingURL=province.test.js.map
--- dist/province.js
+++ dist/province.js
@@ -4,15 +4,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const allSchools_json_1 = __importDefault(require("./assets/allSchools.json"));
+const provinces_json_1 = __importDefault(require("./assets/provinces.json"));
+const lodash_1 = __importDefault(require("lodash"));
+function getProvinces() {
+    return provinces_json_1.default;
+}
 function getSchoolsOfProvince(province_name) {
     if (!province_name)
         return [];
+    let province_name_cap = lodash_1.default.startCase(lodash_1.default.toLower(province_name));
+    console.log(province_name_cap);
     const schools = allSchools_json_1.default.data.filter((value) => {
-        return value.jurisdiction === province_name;
+        return value.jurisdiction === province_name_cap;
+    });
+    return schools;
+}
+function getSchoolsOfProvinceAndLocality(province_name, locality) {
+    if (!province_name)
+        return [];
+    if (!locality)
+        return [];
+    let province_name_cap = lodash_1.default.startCase(lodash_1.default.toLower(province_name));
+    let locality_cap = lodash_1.default.startCase(lodash_1.default.toLower(locality));
+    console.log(province_name_cap);
+    console.log(locality_cap);
+    const schools = allSchools_json_1.default.data.filter((value) => {
+        return value.jurisdiction === province_name_cap && value.locality === locality_cap;
     });
     return schools;
 }
 exports.default = {
+    getProvinces,
     getSchoolsOfProvince,
+    getSchoolsOfProvinceAndLocality
 };
 //# sourceMappingURL=province.js.map
